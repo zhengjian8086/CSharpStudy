4485f8d4bec3474063c137d5f63fd00bf96bc3e6
ZJ <z731000532@hotmail.com>
2023-12-30 23:28:29 +0800

    Init my C# study project

---
+++ Study001/Utils.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study001
+{
+    class Utils
+    {
+        public string CreateDate = DateTime.Now.ToString();
+        string? UnknowString;
+        string? CurrentInstanceName;
+
+        public string GetUnknowString()
+        {
+            return UnknowString;
+        }
+
+        public void SetUnknowString(string unknowString)
+        {
+            UnknowString = unknowString;
+            Console.WriteLine(this.UnknowString);
+        }
+
+        public void SetCurrentInstanceName()
+        {
+            CurrentInstanceName = this.ToString();
+        }
+
+        public string GetCurrentInstanceName()
+        {
+            return CurrentInstanceName;
+        }
+        static void StartHelloWorld(String inputWord = "")
+        {
+            if (inputWord != "")
+            {
+                Console.WriteLine($@"Hello {inputWord} from 'Utils' class");
+            }
+            else
+            {
+                Console.WriteLine("Hello world from 'Utils' class");
+            }
+
+        }
+    }
+}
